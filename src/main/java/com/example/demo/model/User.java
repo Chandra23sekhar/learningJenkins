4803d6253e51f32fa8b4969d16c3df2d56929891
D V S Chandrasekhar <chan23du@gmail.com>
2025-03-21 20:52:14 +0530

    Resolved id not showing in getAllUsers

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -10,66 +10,72 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "users")
 public class User {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	@Column
 	private String name;
-	
+
 	@Column
 	private String phone_number;
-	
+
 	@Column
 	private String city;
-	
+
 	@Column
-	private int age;	
-	
+	private int age;
+
 	@Column
 	private String pincode;
-	
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getName() {
 		return this.name;
 	}
-	
+
 	public void setNumber(String phone_number) {
 		this.phone_number = phone_number;
 	}
-	
-	
+
 	public String getNumber() {
 		return this.phone_number;
 	}
-	
+
 	public void setCity(String city) {
 		this.city = city;
 	}
-	
+
 	public String getCity() {
 		return this.city;
 	}
-	
+
 	public void setAge(int age) {
 		this.age = age;
 	}
-	
+
 	public int getAge() {
 		return this.age;
 	}
-	
+
 	public void setPinCode(String pincode) {
 		this.pincode = pincode;
 	}
-	
+
 	public String getPinCode() {
 		return this.pincode;
 	}
-	
+
 }
